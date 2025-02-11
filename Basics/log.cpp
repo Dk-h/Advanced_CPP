f62e2fd11bdbc6c68313c40bbb5d0f5145a0bb3d
Dhruv <programmingdhruv@gmail.com>
2025-02-11 17:39:19 +0000

    log.cpp doxygen comments

--- Basics/log.cpp
+++ Basics/log.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/**
+ * @brief Logs a message to the standard output.
+ * 
+ * This function takes a C-style string (const char*) as input and prints it to the standard output followed by a newline.
+ * 
+ * @param message The message to be logged.
+ */
 void LOG(const char* message) {
     std::cout << message << std::endl;
 }
